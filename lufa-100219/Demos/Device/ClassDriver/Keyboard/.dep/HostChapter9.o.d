a92b06610131b4253481d21412f943d312c53882
David Whetstone <david@humblehacker.com>
2010-05-07 16:53:30 -0700

    remove intermediate files

--- lufa-100219/Demos/Device/ClassDriver/Keyboard/.dep/HostChapter9.o.d
+++
@@ -1,5 +0,0 @@
-../../../../LUFA/Drivers/USB/LowLevel/HostChapter9.o:  \
- ../../../../LUFA/Drivers/USB/LowLevel/HostChapter9.c \
-  ../../../../LUFA/Drivers/USB/LowLevel/../HighLevel/USBMode.h
-
-../../../../LUFA/Drivers/USB/LowLevel/../HighLevel/USBMode.h:
