11fb00572a8ef5954b61af3df2bedbe23d78dc86
David Whetstone <david@humblehacker.com>
2010-05-26 11:41:12 -0700

    fix printf -> HID report

--- src/hhstdio.c
+++ src/hhstdio.c
@@ -5,46 +5,49 @@ static FILE buf_stdout = FDEV_SETUP_STREAM(hid_putc, NULL, _FDEV_SETUP_WRITE);
 
 #define OUTPUT_BUFSIZE 128
 char output_buffer[OUTPUT_BUFSIZE];
-uint8_t output_current_pos;
+uint8_t current_pos;
+uint8_t end_pos;
 
 void
 init_stdio(void)
 {
-  stdout = &buf_stdout;
-  output_current_pos = 0;
+  stdout      = &buf_stdout;
+  current_pos = 0;
+  end_pos     = 0;
 }
 
 int
 hid_putc(char c, FILE *stream)
 {
-  if (output_current_pos == OUTPUT_BUFSIZE - 1)
+  if (end_pos == OUTPUT_BUFSIZE - 1)
     return -1;
 
-  output_buffer[output_current_pos] = c;
-  output_buffer[++output_current_pos] = '\0';
+  output_buffer[end_pos] = c;
+  output_buffer[++end_pos] = '\0';
   return 0;
 }
 
 bool
 stdout_is_empty(void)
 {
-  return output_current_pos == 0;
+  return end_pos == 0;
 }
 
 char
 popchar(void)
 {
-  if (output_current_pos == OUTPUT_BUFSIZE || !output_buffer[output_current_pos])
+  if (current_pos == OUTPUT_BUFSIZE || !output_buffer[current_pos])
   {
     output_buffer[0] = '\0';
-    output_current_pos = 0;
+    current_pos = 0;
+    end_pos = 0;
     return '\0';
   }
 
-  char ch = output_buffer[output_current_pos];
+  char ch = output_buffer[current_pos];
   if (ch == '\\')
   {
-    switch ((ch = output_buffer[++output_current_pos]))
+    switch ((ch = output_buffer[++current_pos]))
     {
     case 'n':
       ch = (char)10; break;
@@ -56,7 +59,7 @@ popchar(void)
       break;
     }
   }
-  ++output_current_pos;
+  ++current_pos;
   return ch;
 }
 
