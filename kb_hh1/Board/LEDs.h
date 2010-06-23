9d25222601245533b8c46ea0cabbf6ae08c6c119
David Whetstone <david@humblehacker.com>
2010-06-23 10:45:48 -0700

    Add Keyboard HH1

---
+++ kb_hh1/Board/LEDs.h
@@ -0,0 +1,124 @@
+/*
+                   The HumbleHacker Keyboard Project
+                 Copyright © 2008-2010, David Whetstone
+              david DOT whetstone AT humblehacker DOT com
+
+  This file is a part of the HumbleHacker Keyboard Firmware project.
+
+  The HumbleHacker Keyboard Project is free software: you can
+  redistribute it and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software Foundation, either
+  version 3 of the License, or (at your option) any later version.
+
+  The HumbleHacker Keyboard Project is distributed in the
+  hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with The HumbleHacker Keyboard Firmware project.  If not, see
+  <http://www.gnu.org/licenses/>.
+
+  --------------------------------------------------------------------
+
+  This code is based on the LUFA library's LED driver stub, written
+  by Dean Camera.
+
+             LUFA Library
+     Copyright (C) Dean Camera, 2010.
+
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+  --------------------------------------------------------------------
+*/
+
+#ifndef __LEDS_USER_H__
+#define __LEDS_USER_H__
+
+	/* Includes: */
+		#include <avr/io.h>
+
+		// TODO: Add any required includes here
+
+/* Enable C linkage for C++ Compilers: */
+		#if defined(__cplusplus)
+			extern "C" {
+		#endif
+
+	/* Preprocessor Checks: */
+		#if !defined(__INCLUDE_FROM_LEDS_H)
+			#error Do not include this file directly. Include LUFA/Drivers/Board/LEDS.h instead.
+		#endif
+
+	/* Public Interface - May be used in end-application: */
+		/* Macros: */
+			/** LED mask for the first LED on the board. */
+			#define LEDS_LED1        (1<<0)
+
+			/** LED mask for the second LED on the board. */
+			#define LEDS_LED2        (1<<1)
+
+			/** LED mask for the third LED on the board. */
+			#define LEDS_LED3        (1<<2)
+
+			/** LED mask for the fourth LED on the board. */
+			#define LEDS_LED4        (1<<3)
+
+			/** LED mask for all the LEDs on the board. */
+			#define LEDS_ALL_LEDS    (LEDS_LED1 | LEDS_LED2 | LEDS_LED3 | LEDS_LED4)
+
+			/** LED mask for the none of the board LEDs */
+			#define LEDS_NO_LEDS     0
+
+      /** Keyboard specific LED mask names */
+      #define LED_NUM          LEDS_LED1
+      #define LED_CAPS         LEDS_LED2
+      #define LED_SCRL         LEDS_LED3
+
+		/* Inline Functions: */
+		#if !defined(__DOXYGEN__)
+			static inline void LEDs_Init(void)
+			{
+        // Keyboard LEDs are F0-F3
+        DDRF  |= LEDS_ALL_LEDS;  // set pins as outputs
+        PORTF |= LEDS_ALL_LEDS;  // set pins high to prevent sinking (lights off)
+			}
+
+			static inline void LEDs_TurnOnLEDs(const uint8_t LEDMask)
+			{
+				PORTF &= ~LEDMask;
+			}
+
+			static inline void LEDs_TurnOffLEDs(const uint8_t LEDMask)
+			{
+				PORTF |= LEDMask;
+			}
+
+			static inline void LEDs_SetAllLEDs(const uint8_t LEDMask)
+			{
+        PORTF = ((PORTF | LEDS_ALL_LEDS) & ~LEDMask);
+			}
+
+			static inline void LEDs_ChangeLEDs(const uint8_t LEDMask, const uint8_t ActiveMask)
+			{
+        PORTF = ((PORTF | LEDMask) & ~ActiveMask);
+			}
+
+			static inline void LEDs_ToggleLEDs(const uint8_t LEDMask)
+			{
+        PORTF = (PORTF ^ (LEDMask & LEDS_ALL_LEDS));
+			}
+
+			static inline uint8_t LEDs_GetLEDs(void) ATTR_WARN_UNUSED_RESULT;
+			static inline uint8_t LEDs_GetLEDs(void)
+			{
+				return ~(PORTF & LEDS_ALL_LEDS);
+			}
+		#endif
+
+	/* Disable C linkage for C++ Compilers: */
+		#if defined(__cplusplus)
+			}
+		#endif
+
+#endif
---
+++ kb_hh1/matrix_discovery_defs.h
@@ -0,0 +1,68 @@
+#ifndef __MATRIX_DISCOVERY_DEFS_H__
+#define __MATRIX_DISCOVERY_DEFS_H__
+
+#ifndef MATRIX_DISCOVERY_MODE
+#error MATRIX_DISCOVERY_MODE must be defined
+#endif
+
+// PORT A
+  #define USE_PINA0 1
+  #define USE_PINA1 1
+  #define USE_PINA2 1
+  #define USE_PINA3 1
+  #define USE_PINA4 1
+  #define USE_PINA5 1
+  #define USE_PINA6 1
+//#define USE_PINA7 1
+
+// PORT B
+//#define USE_PINB0 1
+//#define USE_PINB1 1
+//#define USE_PINB2 1
+//#define USE_PINB3 1
+//#define USE_PINB4 1
+//#define USE_PINB5 1
+//#define USE_PINB6 1
+//#define USE_PINB7 1
+
+// PORT C
+  #define USE_PINC0 1
+  #define USE_PINC1 1
+  #define USE_PINC2 1
+  #define USE_PINC3 1
+  #define USE_PINC4 1
+  #define USE_PINC5 1
+  #define USE_PINC6 1
+  #define USE_PINC7 1
+
+// PORT D
+  #define USE_PIND0 1
+  #define USE_PIND1 1
+  #define USE_PIND2 1
+  #define USE_PIND3 1
+  #define USE_PIND4 1
+  #define USE_PIND5 1
+  #define USE_PIND6 1
+  #define USE_PIND7 1
+
+// PORT E
+//#define USE_PINE0 1
+//#define USE_PINE1 1
+//#define USE_PINE2 1
+//#define USE_PINE3 1
+//#define USE_PINE4 1
+//#define USE_PINE5 1
+//#define USE_PINE6 1
+//#define USE_PINE7 1
+
+// PORT F
+//#define USE_PINF0 1
+//#define USE_PINF1 1
+//#define USE_PINF2 1
+//#define USE_PINF3 1
+//#define USE_PINF4 1
+//#define USE_PINF5 1
+//#define USE_PINF6 1
+//#define USE_PINF7 1
+
+#endif // __MATRIX_DISCOVERY_DEFS_H__
